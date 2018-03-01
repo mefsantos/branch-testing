74855075660fd3db6d33aa65ea50194c4d1f1270
mefsantos <medgarfsantos@gmail.com>
2018-03-01 13:58:53 +0000

    add setup.py. Start dist util for package distribution

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='myPackage',
+      version='0.1',
+      description='My first python package',
+      author='Marcelo Santos',
+      author_email='email@domain.com',
+      url='https://github.com/mefsantos/branch-testing',
+      packages=['.', 'modules'],
+     )
+
